a54768426a14141696cda52a319c4ac4b2d76daa
Henrique <henriquekkrrr@gmail.com>
2025-11-22 19:20:27 -0300

    escrita de arquivo simples

--- cpp/compress.cpp
+++ cpp/compress.cpp
@@ -3,21 +3,12 @@
 #include <fstream>
 #include <iostream>
 #include <list>
+#include <ostream>
+#include <string>
 #include "arvore.h"
+#include "huffman.h"
 
 using std::size_t;
-
-class HuffmanNode {
-    public:
-        Arvore* arvore;
-        size_t tam;
-
-        HuffmanNode(Arvore* a, size_t t) : arvore(a), tam(t) {}
-};
-
-bool comparaPorTamanho(const HuffmanNode& a, const HuffmanNode& b) {
-    return a.tam < b.tam;
-}
 
 size_t frequencias[256];
 std::list<HuffmanNode> arvoreHuffman;
@@ -28,28 +19,6 @@ void contaFrequencia(size_t freq[], const char buffer[], size_t length) {
         unsigned char byteVal = static_cast<unsigned char>(buffer[i]);
         freq[byteVal]++;
     }
-}
-
-void preencheArvoreInicial(std::list<HuffmanNode>& huff, size_t freq[]) {
-    for (int i = 0; i < 256; i++) {
-        if (freq[i] != 0) {
-            Arvore* newArvore = arv_constroi(static_cast<char>(i), nullptr, nullptr);
-            huff.push_back(HuffmanNode(newArvore, freq[i]));
-        }
-    }
-}
-
-Arvore* gerarTrieHuffman(std::list<HuffmanNode>& huff) {
-    huff.sort(comparaPorTamanho);
-    while (huff.size() > 1) {
-        HuffmanNode n1 = huff.front(); huff.pop_front();
-        HuffmanNode n2 = huff.front(); huff.pop_front();
-        Arvore* newArvore = arv_constroi('\0', n1.arvore, n2.arvore);
-        huff.emplace_back(newArvore, n1.tam + n2.tam);
-        huff.sort(comparaPorTamanho);
-    }
-    if (huff.empty()) return nullptr;
-    return huff.front().arvore;
 }
 
 void printTreeHorizontal(Arvore* node, std::string prefix = "", bool isLeft = true, bool isRoot = true) {
@@ -98,6 +67,77 @@ void gerarCodigos(Arvore* arv, std::string codigos[], std::string codAnterior) {
 
 }
 
+class BitWriter{
+    public:
+        std::ostream &out;
+        unsigned char buffer = 0;
+        int bit_count = 0;
+
+        BitWriter(std::ostream &o) : out(o) {}
+
+        void writeBit(int bit) {
+            // Pega o buffer ex: 0011 faz shift com "<<"
+            // 0000 0011 << 1 = 0000 0110 e o bitwise or coloca o bit no final
+            // 0000 0110 | 0000 0001 = 0111
+            buffer = (buffer << 1) | bit;
+            bit_count++;
+            if (bit_count == 8) {
+                out.put(buffer);
+                buffer = 0;
+                bit_count = 0;
+            }
+        }
+
+        void writeByte(unsigned char b) {
+            if (bit_count == 0) {
+                out.put(b);
+            } else {
+                //Extrai os bits usando shifts e bitwise and
+                //ex: 1100 0000 >> 7 = 0000 0001 & 1 = 1
+                //    1100 0000 >> 5 = 0000 0110 & 1 = 0
+                for (int i = 7; i >=0; i--) {
+                    writeBit((b >> i) & 1);
+                }
+            }
+        }
+
+        void flush() {
+            if (bit_count > 0) {
+                buffer <<= (8 - bit_count);
+                out.put(buffer);
+                buffer = 0;
+                bit_count = 0;
+            }
+        }
+
+};
+
+void escreverTrie(arvore *arv, BitWriter &bw) {
+    if (!arv) return;
+
+    if (!arv->esq && !arv->dir) {
+        //Armazena 1 se nó folha
+        bw.writeBit(1);
+        bw.writeByte((unsigned char) arv->info);
+    } else {
+        //Armazenza 0 caso contrário e percorre a árvore
+        bw.writeBit(0);
+        escreverTrie(arv->esq, bw);
+        escreverTrie(arv->dir, bw);
+    }
+};
+
+void escreverTexto(std::string codigos[], BitWriter &bw, const char buffer[], size_t length) {
+    for (size_t i = 0; i < length; i++) {
+        unsigned char byteVal = static_cast<unsigned char>(buffer[i]);
+        std::string &cod = codigos[byteVal];
+        for (char bitChar : cod) {
+            int bit = (bitChar == '1') ? 1 : 0;
+            bw.writeBit(bit);
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     const size_t BUFFER_SIZE = 1024;
     char buffer[BUFFER_SIZE];
@@ -142,17 +182,56 @@ int main(int argc, char** argv) {
 
     arvore* final = gerarTrieHuffman(arvoreHuffman);
 
-    printTreeHorizontal(final);
+    //printTreeHorizontal(final);
 
     gerarCodigos(final, codigos, "");
 
-    for (int i = 0; i < 256; i++) {
+    /*for (int i = 0; i < 256; i++) {
         if (codigos[i].size() != 0){
             std::cout << static_cast<char>(i) << ", " << "Codigo :" << codigos[i] << '\n';
         }
+    }*/
+
+    std::ofstream outputFile("output");
+
+    BitWriter bw(outputFile);
+
+    escreverTrie(final, bw);
+
+    bw.flush();
+
+    std::ifstream file2(argv[1], std::ios::binary);
+    if (!file2) {
+        std::cerr << "Erro: não foi possível abrir arquivo!\n";
+        return 1;
     }
 
-    
+    while (true) {
+        // Read up to BUFFER_SIZE bytes
+        file2.read(buffer, BUFFER_SIZE);
+        std::streamsize bytesRead = file2.gcount();
+
+        if (bytesRead <= 0) {
+            // No more data to read (EOF or error)
+            break;
+        }
+
+        // Process this chunk of data (here we just write it to stdout)
+        escreverTexto(codigos, bw, buffer, bytesRead);
+
+        // If EOF reached, stop
+        if (file2.eof()) {
+            break;
+        }
+
+        // If some other error occurred, handle it
+        if (!file2 && !file2.eof()) {
+            std::cerr << "\nError while reading file.\n";
+            return 1;
+        }
+    }
+
+    bw.flush();
 
     return 0;
 }
